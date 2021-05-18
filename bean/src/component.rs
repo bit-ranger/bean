8b50cda96b86ae31801c32144ab6398b01934ec6
bit-ranger <sincerebravefight@gmail.com>
2021-05-18 20:38:25 +0800

    it works

---
+++ bean/src/component.rs
@@ -0,0 +1,10 @@
+pub use std::sync::Arc;
+
+pub trait HasComponent<C> {
+
+    fn add(&mut self, name: &str, component: Arc<C>);
+
+    fn get(&self, name: &str) -> Option<Arc<C>>;
+
+    fn get_all(&self) -> Vec<(&str, Arc<C>)>;
+}
